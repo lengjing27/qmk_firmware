51cf8b2c2d8e91806633d7375309d3edb791e5b9
Lei Yu <yuleiz@gmail.com>
2019-08-13 11:56:33 +0800

    add nrf mk files

---
+++ tmk_core/protocol/nrf.mk
@@ -0,0 +1,10 @@
+# for protocol specific files
+
+PROTOCOL_DIR = protocol
+NRF_DIR = $(PROTOCOL_DIR)/nrf
+
+
+SRC += $(NRF_DIR)/main.c
+
+VPATH += $(TMK_PATH)/$(PROTOCOL_DIR)
+VPATH += $(TMK_PATH)/$(NRF_DIR)
